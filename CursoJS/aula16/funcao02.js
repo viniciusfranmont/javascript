a2087d0f55f54f3a50ab617ca857929d987e8c7b
ViniciusFranMont <viniciusfrmonteiro@gmail.com>
2022-03-01 17:32:33 -0300

    Funções em JS

---
+++ CursoJS/aula16/funcao02.js
@@ -0,0 +1,7 @@
+function soma(n1 = 0, n2 = 0) {
+    return n1 + n2;
+}
+
+console.log(`A soma entre 2 e 5 é: ${soma(2, 5)}`);
+console.log(`A soma entre 2 e # é: ${soma(2)}`);
+console.log(`A soma entre # e # é: ${soma()}`);
