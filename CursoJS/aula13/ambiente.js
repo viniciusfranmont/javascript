316718ece685f6b20d3f3949e830077cb3b085fb
ViniciusFranMont <viniciusfrmonteiro@gmail.com>
2022-03-01 14:30:40 -0300

    Aula 13

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1;
+
+while(c <= 6) {
+    console.log(`Passo ${c}`);
+    c++;
+}
+
+var k = 1;
+do {
+    console.log(`Passo k: ${k}`);
+    k++;
+} while (k <= 6);
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
