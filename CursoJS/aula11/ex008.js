a8e6c3693f203c7772f3f73ac0a6cc4687aeb56b
ViniciusFranMont <viniciusfrmonteiro@gmail.com>
2022-03-01 05:56:32 -0300

    Exercícios JS

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,11 @@
+/* Usando a extensão Code Runner, muito boa para rodar várias linguagens. 
+Para dar run no código, Ctrl + Alt + N. */
+
+var vel = 78.2;
+
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if (vel > 60){ //Condição Simples! (Não tem se não)
+    console.log(`Você ultrapassou a velocidade permitida! MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!'); 
+
