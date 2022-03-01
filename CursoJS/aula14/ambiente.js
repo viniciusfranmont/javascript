07955576a90af90afad02212119e68f4852750f7
ViniciusFranMont <viniciusfrmonteiro@gmail.com>
2022-03-01 15:18:27 -0300

    Estruturas de repetição

---
+++ CursoJS/aula14/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1;
+var k = 1;
+
+console.log('Vai começar...');
+
+while (c <= 5){
+    console.log(c);
+    c++;
+}
+
+do {
+    console.log(k);
+    k++;
+} while(k <= 5)
+
+for(var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log('FIM!');
